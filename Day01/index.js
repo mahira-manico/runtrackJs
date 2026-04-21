299ee6187e89dcf2cbee814a74d15f9a33376cd9
mahira-manico <mahira.manico@laplateforme.io>
2026-04-21 13:01:45 +0200

    init: initializating of folder and job 1-4 done

---
+++ Day01/index.js
@@ -0,0 +1,19 @@
+
+//window.alert("This is an alert!"); //pop up alert
+//console.log("omo"); //display in console
+//console.log("i love chocolate!");
+
+//document.getElementById("MyH1").textContent="hello"; //display content in page
+//document.getElementById("myP").textContent="I love pizza";
+
+//let age=25;
+//let price=10.99;
+//let firstName="Mahira";
+
+//console.log(typeof firstName)
+//console.log(price);
+//console.log(age);
+
+//console.log(`You are ${age} years old`);
+//console.log(`The price is ${price}$`);
+//console.log(`Your name is ${firstName}`);
