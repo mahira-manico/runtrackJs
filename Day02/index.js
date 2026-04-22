028cde5d73bfbd730e34cc3e53128a96ee900d51
mahira-manico <mahira.manico@laplateforme.io>
2026-04-22 15:03:49 +0200

    feat: job3 done

--- Day02/index.js
+++ Day02/index.js
@@ -3,4 +3,22 @@ function citation(){
     console.log(word)
 }
 
+function showhide(){
+    let art=document.getElementById("article");
+    if(art.style.display=="none"){
+        art.style.display="block";
+    }else{
+        art.style.display="none";
+    }
+
+}
+
+let count=0;
+let btn=document.getElementById("button");
+let display=document.getElementById("counter");
+btn.addEventListener("click",function addOne(){
+    count++
+    display.innerHTML=count;
+
+});
 
