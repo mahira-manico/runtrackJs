ab6af4a46d23b813d3b2f6284417e73dd4700736
mahira-manico <mahira.manico@laplateforme.io>
2026-04-23 10:34:17 +0200

    feat: job1 of day3 done

---
+++ Day03/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+    $("#btn").click(function showing(){
+        $("p").show();
+    })
+
+    $("#btn2").click(function hiding(){
+        $("p").hide();
+        })
+})
