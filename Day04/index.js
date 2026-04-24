1d0d129ec90ff2db6cd9d46064cc73986cf6ebfb
mahira-manico <mahira.manico@laplateforme.io>
2026-04-24 16:13:23 +0200

    feat: job2 of day4 done

--- Day04/index.js
+++ Day04/index.js
@@ -4,3 +4,15 @@ fetch("expression.txt")
 .then(data=>document.getElementById("p").innerHTML=data)
 .catch(error=>console.error(error));
 }
+
+function jsonValueKey(jsonString, key){
+ 
+    let parsing=JSON.parse(jsonString);
+    let value=parsing[key];
+    return value;
+}
+
+const test='{ "name":"La Plateforme", "address":"8 rue d Hozier", "city":"Marseille", "nb_staff":"11","creation":"2019"}'
+
+let testing=jsonValueKey(test,"name");
+console.log(testing);
